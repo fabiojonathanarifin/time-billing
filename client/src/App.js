32b865390faf3bbff8562d6c38e29c69282a751a
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-10 14:49:21 +0700

    Added DashProfile to main app

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Sidebar from "./layouts/Sidebar";
 import Login from "./pages/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import DashProfile from "./pages/DashProfile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       </Routes>
       <Registration />
       <Dashboard />
+      <DashProfile />
       {/* <div className="contentmargin border-2 border-black md:container sm:mx-auto">
         <h1>Doom</h1>
       </div> */}
