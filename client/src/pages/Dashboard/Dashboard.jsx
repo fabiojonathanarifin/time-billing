c4c98ad96d47639e447fe45809f99b68dd80a51e
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-12 17:00:12 +0700

    Testing Dashboard new Buttons

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
-import AddTime from "./SubItem/AddTime";
+import SelectProject from "../../components/Buttons/SelectProject";
+import AddActivity from "./SubItem/AddActivity";
+import AddClient from "./SubItem/AddClient";
+import AddExpenses from "./SubItem/AddExpenses";
 
 function Dashboard() {
-  return <AddTime />;
+  return (
+    <div className="space-y-6">
+      <AddActivity />
+      <AddExpenses />
+      <AddClient />
+    </div>
+  );
 }
 export default Dashboard;
