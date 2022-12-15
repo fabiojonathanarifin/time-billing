20fff29a76016834e89127b4b6230e5c535bdbbd
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-15 18:01:47 +0700

    Created TeamProfile

---
+++ client/src/pages/TeamProfile/TeamProfile.jsx
@@ -0,0 +1,11 @@
+function TeamProfile() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <h1>Team Logo</h1>
+        <h3>Team Name</h3>
+      </div>
+    </div>
+  );
+}
+export default TeamProfile;
