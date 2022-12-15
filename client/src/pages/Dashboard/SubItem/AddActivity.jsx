dfdafdc57c876ee4e2a682bafb4b162a3f96df4c
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-15 18:01:23 +0700

    fixed AddActivity

--- client/src/pages/Dashboard/SubItem/AddActivity.jsx
+++ client/src/pages/Dashboard/SubItem/AddActivity.jsx
@@ -64,7 +64,7 @@ export default function AddActivity() {
                         htmlFor="project"
                         className="block flex items-start text-sm font-medium text-gray-700 undefined"
                       >
-                        Project
+                        Your Activity
                       </label>
                       <div className="flex flex-col items-start">
                         <input
