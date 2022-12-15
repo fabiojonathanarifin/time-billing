1acf1c01dc22280fb4b810583258ae360f8a9e98
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-15 18:00:03 +0700

    Created Project Table

---
+++ client/src/components/ProjectTable.jsx
@@ -0,0 +1,25 @@
+function ProjectTable() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Billable Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Project 1</td>
+            <td>2000 $</td>
+          </tr>
+          <tr>
+            <td>Project 2</td>
+            <td>20 $/hr</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default ProjectTable;
