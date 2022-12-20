c676e8a5b2c04f13d1ae4e7cdb94c2237dbc08e2
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-20 19:52:43 +0700

    Added Progress bar for daily progress

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import RadialProgressBar from "./RadialProgressBar/RadialProgressBar";
 import "./Users.css";
 
 function Users() {
   return (
     <div className="">
-      <div className="users-table">
+      <div className="mt-2 users-table">
+        <div>Name</div>
+        <div className="users-7days">
+          <div className="day">1</div>
+          <div className="day">2</div>
+          <div className="day">3</div>
+          <div className="day">4</div>
+          <div className="day">5</div>
+          <div className="day">6</div>
+          <div>7</div>
+        </div>
+        <div>Status</div>
+      </div>
+      <div className="mt-2 users-table">
         <div className="card">
           <div className="test w-10 h-10">
             <img
@@ -13,14 +27,64 @@ function Users() {
             />
           </div>
           <div className="user-profile">
-            <div className="font-semibold" id="user-info">
+            <div className="font-semibold" id="user-info-name">
               Fabio Jonathan
             </div>
-            <div id="user-info">Admin | Full-Time | 40 h/wk</div>
+            <div id="user-info-status">Admin | Full-Time | 40 h/wk</div>
           </div>
         </div>
-        <div className="card">ben</div>
-        <div className="card">ble</div>
+        <div className="card users-7days">
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+            <div className="progress-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
+              {/* 72% */}
+            </div>
+          </div>
+        </div>
+        <div className="billing-status card">
+          <div className="flex flex-col px-2 items-center">
+            Billable:
+            <div className="mt-1">
+              <RadialProgressBar />
+            </div>
+          </div>
+          <div className="flex flex-col px-2 items-center">
+            <div>Non-Billable:</div>
+            <div className="mt-1 ">
+              <RadialProgressBar />
+            </div>
+          </div>
+          <div>Capacity Left: 7 hr/wk</div>
+        </div>
       </div>
 
       <div className="mt-2 users-table">
