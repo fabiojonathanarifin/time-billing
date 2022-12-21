2fad0274e07a40bacc5bc1b8c330e061428f3439
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-12-21 19:09:04 +0700

    updated sub-sidebar

--- client/src/layouts/Sidebar.jsx
+++ client/src/layouts/Sidebar.jsx
@@ -1,5 +1,4 @@
 import "./layout.css";
-import { Menu } from "@headlessui/react";
 import {
   HomeIcon,
   ChartBarIcon,
@@ -11,32 +10,40 @@ import {
 function Sidebar() {
   return (
     <div className="sidebar bg-gray-800 flex flex-col justify-between">
-      <div>
-        <div className="mt-3">
+      <div className="w-full">
+        <div className="mt-3  inline-block">
           <img
             className="block h-10 w-auto"
             src="/logo.png"
             alt="Your Company"
           />
         </div>
-        <div className="mt-12 space-y-8 menu justify-between">
+        <div className="w-full mt-12 space-y-8 relative bg-gray-800">
           <div>
-            <HomeIcon className="icon-color" />
+            <HomeIcon className="sidebar-button" />
+            <div className="sidebar-button-content bg-gray-700 rounded-md">
+              <div className="sub font-bold">Dashboard</div>
+              <div className="sub mt-2 space-y-1 pl-3 font-semibold">
+                <p>Activity</p>
+                <p>Timer</p>
+                <p>Toggle</p>
+              </div>
+            </div>
           </div>
           <div>
-            <ChartBarIcon className="icon-color" />
+            <ChartBarIcon className="sidebar-button" />
           </div>
           <div>
-            <BriefcaseIcon className="icon-color" />
+            <BriefcaseIcon className="sidebar-button" />
           </div>
           <div>
-            <Cog6ToothIcon className="icon-color" />
+            <Cog6ToothIcon className="sidebar-button" />
           </div>
         </div>
       </div>
       <div>
         <div>
-          <FlagIcon className="p-1 mb-3 icon-color" />
+          <FlagIcon className="p-1 mb-3 sidebar-button" />
         </div>
         <div>
           <img
