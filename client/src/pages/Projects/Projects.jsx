070c04d0705c326fa71278edaaccdf964566bf1e
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2023-01-15 00:52:26 +0700

    added Projects

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -4,14 +4,18 @@ import Project from "./Project";
 
 function Projects() {
   return (
-    <div className="mt-12 w-3/4 mx-auto">
-      <div className="invoice-bar py-2.5 px-6 text-gray-600">
-        <div>Client</div>
-        <div>Hours Spent</div>
-        <div>Budget</div>
-        <div>Spent</div>
-        <div>Budget Remaining</div>
-        <div>Expenses</div>
+    <div className="mt-12 w-10/12 mx-auto">
+      <div className="project-bar py-2.5 px-6 text-gray-600 text-light">
+        <div>
+          <div>Client</div>
+        </div>
+        <div className="project-content">
+          <div>Hours Spent</div>
+          <div>Budget</div>
+          <div>Spent</div>
+          <div>Budget Remaining</div>
+          <div>Expenses</div>
+        </div>
       </div>
       <div className="grid gap-y-3">
         <Project />
