b3eed814820fd10db8d27f87f601056044188356
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2023-01-16 18:08:40 +0700

    Added Grid content to dashboard

--- client/src/pages/Teams/TeamsDashboard.jsx
+++ client/src/pages/Teams/TeamsDashboard.jsx
@@ -1,4 +1,55 @@
+import "./Teams.css";
 function TeamsDashboard() {
-  return <div>TeamsDashboard</div>;
+  return (
+    <div className="mt-12 mx-auto flex">
+      <div className="teams-card">
+        <div className="teams-card-head">
+          <div className="py-3 space-x-2 teams-card-content border-b-2  border-slate-300">
+            <img
+              className="h-12 w-12 rounded-lg"
+              src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+              alt=""
+            />
+            <div className="">
+              <div className="flex font-semibold text-base">Brown Team</div>
+              <div className="flex">Development</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className="teams-card-head">
+              <div className="py-3 space-x-2 teams-card-content">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+                  alt=""
+                />
+                <div className="">
+                  <div className="flex font-semibold text-base">Milo</div>
+                  <div className="flex">Developer | Full-Time</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="teams-card-head">
+              <div className="py-3 space-x-2 teams-card-content">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1608831540955-35094d48694a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=526&q=80"
+                  alt=""
+                />
+                <div className="">
+                  <div className="flex font-semibold text-base">Lola</div>
+                  <div className="flex">UI/UX Designer | Contractor</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default TeamsDashboard;
