ed53dd614ceaea9cc02cb74707c640b1d6ca4204
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2023-01-16 18:08:24 +0700

    Fixed Project Content UI

--- client/src/pages/Projects/Project.jsx
+++ client/src/pages/Projects/Project.jsx
@@ -4,12 +4,12 @@ function Project() {
   return (
     <div className="project-bar card font-medium text-sm">
       <div className="client">
-        <div className="font-boldcd text-base">Skitraw Tech</div>
-        <div>TimeBilling</div>
+        <div className="font-semibold text-lg">Skitraw Tech</div>
+        <div className="font-normal">TimeBilling</div>
       </div>
-      <div className="project-content">
-        <div>40 hours 20 minutes</div>
-        <div>80 hours</div>
+      <div className="project-content text-sm">
+        <div>40h 20m</div>
+        <div>80h</div>
         <div className="spent-bar-grid ">
           <div className="w-full h-3 bg-gray-200 rounded-full dark:bg-gray-700">
             <div className="spent-bar bg-sky-600 text-xs font-medium text-blue-100 text-end p-0.5 leading-none rounded-full">
@@ -18,8 +18,8 @@ function Project() {
           </div>
           53%
         </div>
-        <div>39 hours 40 minutes</div>
-        <div>$400</div>
+        <div className="text-green-600">39h 40m</div>
+        <div className="text-red-600">$400</div>
         <div>
           <PencilSquareIcon className="h-5 w-5 text-blue-500" />
         </div>
